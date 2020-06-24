01deb01bb1e9f5d8dd4f23ee40eb2aa05a55906a
amayacausey <amayacausey@gmail.com>
2020-06-24 07:35:57 -0700

    fix loop

--- src/IffyLoop.java
+++ src/IffyLoop.java
@@ -4,40 +4,18 @@ import java.util.Scanner;
 public class IffyLoop {
 
     public static void main (String [] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
 
-        String[] random = new String [5];
-        for(int r=0;r <random.length;r++){
-            System.out.println("Please enter 5 random words. Word 0"+(r+1));
-            random[r]=sc.nextLine();
+        String[] random = new String[5];
+        for (int r = 0; r < random.length; r++) {
+            System.out.println("Please enter 5 random words. Word 0" + (r + 1));
+            random[r] = sc.nextLine();
         }
 
 
         System.out.println("5 Random Words : " + Arrays.toString(random));
-
-        String userInput = sc.nextLine();
-        boolean redEyes;
-
-        System.out.println("Are your eyes red?");
-        if ((userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y")))
-        {
-            redEyes=true;
-
-        }else
-        {
-            redEyes=false;
-        }
-        if (redEyes==true)
-        {
-            System.out.println("Get some sleep! ");
-        }else
-        {
-            System.out.println("You look great! ");
-        }
-
-
-
+        System.out.println("Press Enter to start the next Program");
        /* while(userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y"))
         {
             System.out.println("Get some sleep! ");
@@ -49,26 +27,28 @@ public class IffyLoop {
             break;
         }
         */
-
-
-
-        System.out.println("Do you want to try again? Enter n if not");
+       // System.out.println("Do you want to try again? Enter n if not");
+        String userInput = sc.nextLine();
+        boolean redEyes;
         String reTry = "";
 
         while (!reTry.equals("n")) {
-            reTry = sc.nextLine();
+            System.out.println("Are your eyes red?");
+            userInput = sc.nextLine();
+            if ((userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y"))) {
+                redEyes = true;
+
+            } else {
+                redEyes = false;
+            }
+            if (redEyes == true) {
+                System.out.println("Get some sleep! ");
+            } else {
+                System.out.println("You look great! ");
+            }
+
+
             System.out.println("Do you want to try again? Enter n if not");
-            System.out.println(reTry);
+            reTry = sc.nextLine();
 
-
-
-        }
-        {
-
-        }
-
-    }
-
-
-
-}
+        }}}
